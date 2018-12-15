835983d4ec2c61a5c4c34ee5812d2127249c1064
f4tp <mr_treadwell@live.co.uk>
2018-12-15 14:38:56 +0000

    Finished video 28 Entity framework @Todo: video 29 onwards

--- VidlyDec2018/Models/Customer.cs
+++ VidlyDec2018/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,14 @@ namespace VidlyDec2018.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MebershipType { get; set; }
+        //below - as naming convention is used, this will be a foreign key in this DB
+        public byte MemberShipTypeId{ get; set; }
 
-        
+
     }
 }
