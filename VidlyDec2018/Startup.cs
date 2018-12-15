859213c4242e67908d628b02680af28c45561a6f
f4tp <mr_treadwell@live.co.uk>
2018-12-15 13:28:20 +0000

    Add project files.

---
+++ VidlyDec2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyDec2018.Startup))]
+namespace VidlyDec2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
